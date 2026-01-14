1d75fac89688be17c72b99758638c7b940c95e55
HM-19BFS <hamzboufous@gmail.com>
2026-01-14 05:07:25 +0100

    en supprime self.student[]
    chaque étudiant appartient forcément à un groupe danc pour trouver un utidiant il suffit de parcourir tous les groups

--- major.py
+++ major.py
@@ -1,7 +1,6 @@
 class Major :
     def __init__(self, name):
         self.name = name
-        self.students = []
         self.courses = []
         self.groups=[] # Liste d'objets de type Groupe
         self.schedule = None
