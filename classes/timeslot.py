dfa74ccb892abfa35b24f571ca0cdf86d9e31c9c
AbarraSaad <153920304+abarrasaad@users.noreply.github.com>
2026-02-01 23:35:55 +0100

    Updated timeslot class

--- classes/timeslot.py
+++ classes/timeslot.py
@@ -3,4 +3,13 @@ class TimeSlot:
         self.day = day
         self.start_time = start_time
         self.end_time = end_time
-            
+    
+    def format_time(self, decimal_time):
+        hours = int(decimal_time)
+        minutes = int((decimal_time - hours) * 60)
+        return f"{hours:02d}:{minutes:02d}"
+
+    def __str__(self):
+        start_str = self.format_time(self.start_time)
+        end_str = self.format_time(self.end_time)
+        return f"[{self.day} | {start_str} - {end_str}]"
